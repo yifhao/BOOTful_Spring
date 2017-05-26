7ec1626e46d6af20df2da0b5d5bbacfad7e06c9a
haoyifen <526667233@qq.com>
2017-05-26 11:56:00 +0800

    first blood

---
+++ bootful/src/main/java/com/jully/BootfulApplication.java
@@ -0,0 +1,12 @@
+package com.jully;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootfulApplication.class, args);
+	}
+}
---
+++ bootful/src/test/java/com/jully/BootfulApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jully;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootfulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
